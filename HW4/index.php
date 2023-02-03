ff957a9a53b7ecd061c92d82ca62c4f4b056dd03
ooze <grigorianna@gmail.com>
2023-02-03 18:35:28 +0400

    Added index.php homeWork_version

--- HW4/index.php
+++ HW4/index.php
@@ -22,6 +22,24 @@ $skills = [
         'percent' => 90,
     ],
 ];
+$experience = [
+    [
+        'name' => 'Мурополк',
+        'date' => 'Jan 2015 - настоящее время',
+        'discription' => 'Мурчал на всю Ивановскую'
+    ],
+    [
+        'name' => 'Шерстиком',
+        'date' => 'Mart 2012 - Dec 2014',
+        'discription' => 'Отрыгивал шерстяные комочки'
+    ],
+    [
+        'name' => 'Вездессун',
+        'date' => 'Jan 2010 - Mart 2012',
+        'discription' => 'Ссал гостям в ботинки'
+    ],
+    
+];
 ?>
 <!DOCTYPE html>
 <html>
@@ -106,26 +124,15 @@ $skills = [
         <div class="w3-container w3-card w3-white w3-margin-bottom">
           <h2 class="w3-text-grey w3-padding-16"><i
               class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+          
+          <?php for ($i = 0; $i < count($experience); $i++):?>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span
-                class="w3-tag w3-teal w3-round">Current</span></h6>
-            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+            <h5 class="w3-opacity"><b><?php echo $experience[$i]['name'];?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $experience[$i]['date'];?></h6>
+            <p><?php echo $experience[$i]['discription'];?></p>
             <hr>
           </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-            <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-            <hr>
-          </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-          </div>
+          <?php endfor;?>
         </div>
  
         <div class="w3-container w3-card w3-white">
