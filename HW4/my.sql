658a146384934994a8597f851c95e6d4f0d0f7ef
ooze <grigorianna@gmail.com>
2023-02-04 13:04:20 +0400

    Bugs fixed

--- HW4/my.sql
+++ HW4/my.sql
@@ -8,25 +8,25 @@ CREATE TABLE IF NOT EXISTS users(
 
 -- insert  
 INSERT INTO users (name, age, address)
-VALUES ('Иван', '18', 'Архангельск');
+VALUES ('Иван', 18, 'Архангельск');
 INSERT INTO users (name, age, address)
-VALUES ('Петр', '18', 'Москва');
+VALUES ('Петр', 18, 'Москва');
 INSERT INTO users (name, age, address)
-VALUES ('Сидр', '33', 'Ростов');
+VALUES ('Сидр', 33, 'Ростов');
 INSERT INTO users (name, age, address)
-VALUES ('Алекс', '50', 'Киров');
+VALUES ('Алекс', 50, 'Киров');
 INSERT INTO users (name, age, address)
-VALUES ('Гриша', '20', 'Москва');
+VALUES ('Гриша', 20, 'Москва');
 INSERT INTO users (name, age, address)
-VALUES ('Витя', '19', 'Архангельск');
+VALUES ('Витя', 19, 'Архангельск');
 INSERT INTO users (name, age, address)
-VALUES ('Егор', '26', 'Москва');
+VALUES ('Егор', 26, 'Москва');
 INSERT INTO users (name, age, address)
-VALUES ('Кир', '30', 'Москва');
+VALUES ('Кир', 30, 'Москва');
 INSERT INTO users (name, age, address)
-VALUES ('Игорь', '55', 'Вологда');
+VALUES ('Игорь', 55, 'Вологда');
 INSERT INTO users (name, age, address)
-VALUES ('Вова', '28', 'Владимир');
+VALUES ('Вова', 28, 'Владимир');
 
 -- fetch 
-SELECT name FROM users WHERE address = 'Москва' AND age>17 AND age<30
+SELECT name FROM users WHERE address = 'Москва' AND age > 17 AND age < 30;
